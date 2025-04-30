791747a2da820dfc61e9416265f10b44c983df55
Hessvacio Hassan <madeinbrooklyngames@gmail.com>
2025-04-30 02:56:20 -0400

    adds assignment structure

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function toggleDarkMode() {
+    // This function should toggle the dark mode class on the body
+    // comment out the alert below and replace it with your code
+    alert("change color in this function");
+  /*
+    TODO: Replace the alert above with code that toggles
+    the 'dark' class on <body>.
+  */
+}
+
+document
+  .getElementById("toggle-btn")
+  .addEventListener("click", toggleDarkMode);
