f51490329e004d7aeb3ada621faf39e1b3132fb5
alejo <alejo@MacBookPro.lan>
2025-05-03 22:27:05 -0400

    dark more enable

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 function toggleDarkMode() {
     // This function should toggle the dark mode class on the body
     // comment out the alert below and replace it with your code
-    alert("change color in this function");
+    // alert("change color in this function");
+    document.body.classList.toggle('dark') 
+    
+    
+    
   /*
     TODO: Replace the alert above with code that toggles
     the 'dark' class on <body>.
@@ -9,5 +13,5 @@ function toggleDarkMode() {
 }
 
 document
-  .getElementById("toggle-btn")
-  .addEventListener("click", toggleDarkMode);
+.getElementById("toggle-btn")
+.addEventListener("click", toggleDarkMode);
